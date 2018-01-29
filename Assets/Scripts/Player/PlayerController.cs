1decaf8c089d0edc32563e86909798318ed892fd
Arctic Crow (Daniel) <arctic-crow@web.de>
2018-01-29 06:01:19 +0100

    PlayerController 1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject GolfBall;
+
+	public float appliedForce = 20f;
+	public float maxForce = 100f;
+
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (GolfBall == null)
+			return;
+
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 dir = new Vector3();
+		dir.z = Input.GetAxis("Vertical");
+		dir.x = Input.GetAxis("Horizontal");
+
+		rb.AddForce(dir * appliedForce);
+	}
+
+	private void LateUpdate ()
+	{
+		//transform.position = GolfBall.transform.position;
 	}
 }
