fdf1ea56b96b8fb55660ecf8aa1f08fa07c80d06
Arctic Crow (Daniel) <arctic-crow@web.de>
2018-02-04 01:31:20 +0100

    First try at adding procedural meshes, might have failed a little
    https://gyazo.com/4416484b047f7ca4811b9df9b69192c7

--- Assets/Scripts/Course/CourseGenerator.cs
+++ Assets/Scripts/Course/CourseGenerator.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using Random = UnityEngine.Random;
 
 public class CourseGenerator : MonoBehaviour {
 
@@ -15,12 +18,11 @@ public class CourseGenerator : MonoBehaviour {
 	public bool useRandomSeed = true;	// Should the randomizer use a random or a given seed?
 	public string seed = "";    // The seed to initialize the randomizer with
 
+	public Material groundMaterial;
+
 
 	[SerializeField]
-	Dictionary<int, Mesh> courseMesh;
-
-	[SerializeField]
-	List<LineRenderer> lineList;
+	List<GameObject> holeList;
 
 	[SerializeField]
 	Vector3[] totalAreaBounds;
@@ -41,11 +43,103 @@ public class CourseGenerator : MonoBehaviour {
 		// Calculate CLOSE BOTTOM LEFT and FAR TOP RIGHT corner of total area
 		CalculateTotalArea();
 
-		// Init line renderers
+		// Create line renderers
 		GenerateLineRenderers();
 
-		// Iterate over position
+		// Create mesh for each line
+		GenerateHoleMeshes();
 
+		// Customize mesh appearence/collision
+		AddMeshRenderers();
+		AddMeshColliders();
+	}
+
+	private void AddMeshColliders ()
+	{
+		for (int i = 0; i < holeList.Count; i++)
+		{
+			GameObject hole = holeList [i];
+			MeshCollider mc = hole.AddComponent<MeshCollider>();
+		}
+	}
+
+	private void AddMeshRenderers ()
+	{
+		for (int i = 0; i < holeList.Count; i++)
+		{
+			GameObject hole = holeList [i];
+			MeshRenderer mr = hole.AddComponent<MeshRenderer>();
+			mr.material = groundMaterial;
+		}
+	}
+
+	private void GenerateHoleMeshes ()
+	{
+		for (int i = 0; i < holeList.Count; i++)
+		{
+			GameObject hole = holeList [i];
+			MeshFilter mf = hole.AddComponent<MeshFilter>();
+			Mesh newMesh = new Mesh();
+
+			LineRenderer lr = hole.GetComponent<LineRenderer>();
+
+			for (int j = 0; j < lr.positionCount - 1; j++)
+			{
+				Vector3 closeLeft = lr.GetPosition(i) - new Vector3(3, 0, 3);
+				Vector3 farRight = lr.GetPosition(i+1) + new Vector3(3, 0, 3);
+
+				// FIXME wrong offset values; see notes on how to fix
+				Vector3 [ ] vertices = new Vector3 [4];
+				vertices [0] = closeLeft;
+				vertices [1] = closeLeft + new Vector3 (6, 0, 0);
+				vertices [2] = farRight - new Vector3(-6, 0, 0);
+				vertices [3] = farRight;
+
+				newMesh.vertices = vertices;
+
+
+				int [ ] tri = new int [6];
+				tri [0] = 0;
+				tri [1] = 2;
+				tri [2] = 1;
+
+				tri [0] = 2;
+				tri [1] = 3;
+				tri [2] = 1;
+
+				newMesh.triangles = tri;
+
+				// FIXME follow up error on wrong offset values; fix afterwards by calculating the negative forward from the plane of vertices
+				Vector3 [ ] normals = new Vector3 [4];
+				normals [0] = -Vector3.forward;
+				normals [1] = -Vector3.forward;
+				normals [2] = -Vector3.forward;
+				normals [3] = -Vector3.forward;
+
+				newMesh.normals = normals;
+
+
+				Vector2 [ ] uv = new Vector2 [4];
+
+				uv [0] = new Vector2(0, 0);
+				uv [1] = new Vector2(1, 0);
+				uv [2] = new Vector2(0, 1);
+				uv [3] = new Vector2(1, 1);
+
+				newMesh.uv = uv;
+			}
+
+			mf.mesh = newMesh;
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			Start();
+		}
 	}
 
 	void CalculateTotalArea()
@@ -62,11 +156,17 @@ public class CourseGenerator : MonoBehaviour {
 
 	void GenerateLineRenderers()
 	{
-		lineList = new List<LineRenderer>();
+
+		// TODO Restrict angles for left/right and up/down
+		// TODO Set minimum/maximum offset between each point
+		holeList = new List<GameObject>();
 		for (int i = 0; i < numberOfHoles; i++)
 		{
 			GameObject newHole = new GameObject("Hole " + (i+1));
 			newHole = Instantiate(newHole, transform);
+
+			newHole.transform.position = GetAreaStart(i) + Vector3.Scale(maxAreaPerHole, new Vector3(0.5f, 1, 0.5f));
+
 			// Set and increment the number of points in line for hole i
 			int points = currentPointAmount;
 			currentPointAmount = Mathf.CeilToInt(Mathf.Pow(currentPointAmount, difficultyIncreasePower));
@@ -81,7 +181,7 @@ public class CourseGenerator : MonoBehaviour {
 				newLine.SetPosition(j, point);
 			}
 
-			lineList.Add(newLine);
+			holeList.Add(newHole);
 		}
 	}
 	
@@ -106,13 +206,5 @@ public class CourseGenerator : MonoBehaviour {
 		Vector3 areaStart = totalAreaBounds [0] + new Vector3(maxAreaPerHole.x + horizontalOffset, 0, 0) * areaIndex;
 
 		return areaStart;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(1))
-		{
-			Start();
-		}
 	}
 }
